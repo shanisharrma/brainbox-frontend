f70c2444ae12b7933304f2ef10de3fbe8f6bc74a
codershani <codershani13@gmail.com>
2024-12-08 12:24:46 +0530

    fix: Resolve sssets file path and icon

--- src/components/core/HomePage/TimelineSection.jsx
+++ src/components/core/HomePage/TimelineSection.jsx
@@ -1,27 +1,27 @@
-import logo1 from "../../../assets/TimeLineLogo/logo1.svg";
-import logo2 from "../../../assets/TimeLineLogo/logo2.svg";
-import logo3 from "../../../assets/TimeLineLogo/logo3.svg";
-import logo4 from "../../../assets/TimeLineLogo/logo4.svg";
-import TimelineImage from "../../../assets/Images/TimelineImage.png";
+import * as Icon1 from "react-icons/sl";
+import * as Icon2 from "react-icons/fa";
+import * as Icon3 from "react-icons/io5";
+import * as Icon4 from "react-icons/hi2";
+import TimelineImage from "@/assets/Images/TimelineImage.png";
 
 const timelineData = [
   {
-    logo: logo1,
+    logo: "SlBadge",
     heading: "Leadership",
     description: "Fully committed to the success company",
   },
   {
-    logo: logo2,
+    logo: "FaGraduationCap",
     heading: "Responsibility",
     description: "Students will always be our top priority",
   },
   {
-    logo: logo3,
+    logo: "IoDiamond",
     heading: "Flexibility",
     description: "The ability to switch is an important skills",
   },
   {
-    logo: logo4,
+    logo: "HiCodeBracketSquare",
     heading: "Solve the problem",
     description: "Code your way to a solution",
   },
@@ -32,21 +32,28 @@ const TimelineSection = () => {
     <div>
       <div className="flex flex-row gap-14 items-center">
         <div className="flex flex-col w-[45%] gap-5">
-          {timelineData.map((element, index) => (
-            <div key={index} className="flex flex-row gap-5 relative">
-              <div className="w-[50px] h-[50px] bg-white flex items-center justify-center rounded-full">
-                <img src={element.logo} alt={element.heading} />
-              </div>
+          {timelineData.map((element, index) => {
+            let Icon =
+              Icon1[element.logo] ||
+              Icon2[element.logo] ||
+              Icon3[element.logo] ||
+              Icon4[element.logo];
+            return (
+              <div key={index} className="flex flex-row gap-5 relative">
+                <div className="w-[50px] h-[50px] bg-white flex items-center justify-center rounded-full">
+                  <Icon size={24} />
+                </div>
 
-              <div>
-                <h3 className="font-semibold text-lg">{element.heading}</h3>
-                <p className="text-base">{element.description}</p>
+                <div>
+                  <h3 className="font-semibold text-lg">{element.heading}</h3>
+                  <p className="text-base">{element.description}</p>
+                </div>
+                {index !== timelineData.length - 1 && (
+                  <div className="h-[50px] w-[1px] bg-rich-black-50 absolute left-2 top-[65px]"></div>
+                )}
               </div>
-              {index !== timelineData.length - 1 && (
-                <div className="h-[50px] w-[1px] bg-rich-black-50 absolute left-2 top-[65px]"></div>
-              )}
-            </div>
-          ))}
+            );
+          })}
         </div>
 
         <div className="relative shadow-blue-200">
